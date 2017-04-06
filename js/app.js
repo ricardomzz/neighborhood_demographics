bc08dfe53156ee5a5f0bdb3babc85f29de7ae337
Ricardo Mazzini <ricardomzz@gmail.com>
2017-04-06 16:20:17 -0400

    Added:
    -Location class
    -Initial list of Locations
    -View showing a list of names for each location

---
+++ js/app.js
@@ -0,0 +1,56 @@
+var initial_locations = [
+  {
+    name: 'Theatro da Paz',
+    lat: -1.4530,
+    lng: -48.4937
+  },
+  {
+    name: 'Mangal das Garças',
+    lat: -1.4649,
+    lng: -48.5055
+  },
+  {
+    name: 'Ver-O-Peso',
+    lat: -1.4524,
+    lng: -48.5038
+  },
+  {
+    name: 'Forte do Castelo',
+    lat: -1.4543,
+    lng: -48.5052
+  },
+  {
+    name: 'Museu Emilio Goeldi',
+    lat: -1.4527,
+    lng: -48.4765
+  },
+  {
+    name: 'Catedral Nossa Senhora de Belém',
+    lat: -1.4561,
+    lng: -48.5048
+  },
+  {
+    name: 'Parque da Residência',
+    lat: -1.4519,
+    lng: -48.4734
+  }
+]
+
+var Location=function(data){
+  this.name = data.name;
+  this.lat = data.lat;
+  this.lng = data.lng;
+}
+
+
+var ViewModel = function(){
+  var self=this;
+  
+
+  this.locationList = ko.observableArray([])
+  initial_locations.forEach(function(locationItem){
+    self.locationList.push(new Location(locationItem))
+  })
+}
+
+ko.applyBindings(new ViewModel());
