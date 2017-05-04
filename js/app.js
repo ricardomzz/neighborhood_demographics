17ecf15da038c3215df5f97061583c7c8c686c3b
Ricardo Mazzini <ricardomzz@gmail.com>
2017-05-04 15:23:49 -0400

    Rewrote most of app. Now includes filtering, animations and infoWindows

--- js/app.js
+++ js/app.js
@@ -1,59 +1,113 @@
-var initial_locations = [
-  {
-    name: 'Theatro da Paz',
-    lat: -1.4530,
-    lng: -48.4937
-  },
-  {
-    name: 'Mangal das Garças',
-    lat: -1.4649,
-    lng: -48.5055
-  },
-  {
-    name: 'Ver-O-Peso',
-    lat: -1.4524,
-    lng: -48.5038
-  },
-  {
-    name: 'Forte do Castelo',
-    lat: -1.4543,
-    lng: -48.5052
-  },
-  {
-    name: 'Museu Emilio Goeldi',
-    lat: -1.4527,
-    lng: -48.4765
-  },
-  {
-    name: 'Catedral Nossa Senhora de Belém',
-    lat: -1.4561,
-    lng: -48.5048
-  },
-  {
-    name: 'Parque da Residência',
-    lat: -1.4519,
-    lng: -48.4734
+
+
+function AppViewModel() {
+
+  var self = this;
+
+  self.initialLocations = [
+    {
+      name: 'Theatro da Paz',
+      lat: -1.4530,
+      lng: -48.4937
+    },
+    {
+      name: 'Mangal das Garças',
+      lat: -1.4649,
+      lng: -48.5055
+    },
+    {
+      name: 'Ver-O-Peso',
+      lat: -1.4524,
+      lng: -48.5038
+    },
+    {
+      name: 'Forte do Castelo',
+      lat: -1.4543,
+      lng: -48.5052
+    },
+    {
+      name: 'Museu Emilio Goeldi',
+      lat: -1.4527,
+      lng: -48.4765
+    },
+    {
+      name: 'Catedral Nossa Senhora de Belém',
+      lat: -1.4561,
+      lng: -48.5048
+    },
+    {
+      name: 'Parque da Residência',
+      lat: -1.4519,
+      lng: -48.4734
+    }
+  ];
+
+
+
+
+  self.filter = ko.observable("");
+  self.locationList = ko.observableArray([]);
+
+  self.filteredLocationList = ko.computed(function(){
+    return ko.utils.arrayFilter(
+      self.locationList(),function(location){
+        return location().name.toUpperCase().includes(self.filter().toUpperCase())
+      }
+    )
   }
-]
 
-var Location=function(data){
-  this.name = data.name;
-  this.lat = data.lat;
-  this.lng = data.lng;
+  );
+
+//return location.name.toUpperCase().includes(self.filter().toUpperCase())
+  self.initialLocations.forEach(function(location){
+    self.locationList.push(ko.observable({
+      name:location.name,
+      lat:location.lat,
+      lng:location.lng,
+      marker:new google.maps.Marker({position:{lat:location.lat,lng:location.lng},map:map}),
+      infoWindow: new google.maps.InfoWindow({content: location.name}),
+      turnOffMarker: function(){this.marker.setMap(null)},
+      bounceMarker: function(){
+
+        marker=this.marker
+
+        //close all infowidows
+        self.locationList().forEach(function(location){location().infoWindow.close()})
+        //open infowindow for selected marker
+        this.infoWindow.open(map,marker)
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        turnOffAnimation=function(){marker.setAnimation(null)}
+        setTimeout(turnOffAnimation,700);
+        }
+
+    }))
+
+  });
+  self.refreshMap = function (){
+    self.locationList().forEach(function (location) {
+      location().marker.setMap(null)
+    });
+    self.filteredLocationList().forEach(function (location) {
+      location().marker.setMap(map)
+    });
+
+  }
+
+  self.filterList = function (formElement){
+    self.filter(formElement.keyword.value);
+    self.refreshMap();
+
+  };
 }
 
-var markers=[]
-var ViewModel = function(){
-  var self=this;
 
-
-  this.locationList = ko.observableArray([])
-  this.markers = []
-  initial_locations.forEach(function(locationItem){
-    self.locationList.push(new Location(locationItem)),
-    markers.push({lat:locationItem.lat,lng:locationItem.lng})
-
-  })
+function initApp() {
+  var belem={lat: -1.455833, lng: -48.503889}
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: belem,
+    mapTypeId: 'terrain',
+    draggable:true,
+    zoom: 12
+  });
+ko.applyBindings(new AppViewModel());
 }
-
-ko.applyBindings(new ViewModel());
