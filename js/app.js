5a84fd488d8b73d27bb125e598d9339b8bb5cd08
Ricardo Mazzini <ricardomzz@gmail.com>
2017-05-04 20:25:19 -0400

    Added: Demographics API, error handling, bootstrap

--- js/app.js
+++ js/app.js
@@ -6,39 +6,29 @@ function AppViewModel() {
 
   self.initialLocations = [
     {
-      name: 'Theatro da Paz',
-      lat: -1.4530,
-      lng: -48.4937
+      name: 'Museum of the Moving Image',
+      lat: 40.7563,
+      lng: -73.9239
     },
     {
-      name: 'Mangal das Garças',
-      lat: -1.4649,
-      lng: -48.5055
+      name: 'Sweet Afton',
+      lat: 40.7654,
+      lng: -73.9191
     },
     {
-      name: 'Ver-O-Peso',
-      lat: -1.4524,
-      lng: -48.5038
+      name: 'Seva Indian Cuisine',
+      lat: 40.7654,
+      lng: -73.9190
     },
     {
-      name: 'Forte do Castelo',
-      lat: -1.4543,
-      lng: -48.5052
+      name: 'Katch',
+      lat: 40.7672,
+      lng: -73.9201
     },
     {
-      name: 'Museu Emilio Goeldi',
-      lat: -1.4527,
-      lng: -48.4765
-    },
-    {
-      name: 'Catedral Nossa Senhora de Belém',
-      lat: -1.4561,
-      lng: -48.5048
-    },
-    {
-      name: 'Parque da Residência',
-      lat: -1.4519,
-      lng: -48.4734
+      name: 'Matsu',
+      lat: 40.7657,
+      lng: -73.9184
     }
   ];
 
@@ -74,10 +64,50 @@ function AppViewModel() {
         //close all infowidows
         self.locationList().forEach(function(location){location().infoWindow.close()})
         //open infowindow for selected marker
-        this.infoWindow.open(map,marker)
+
         marker.setAnimation(google.maps.Animation.BOUNCE);
         turnOffAnimation=function(){marker.setAnimation(null)}
         setTimeout(turnOffAnimation,700);
+        setWindowContent=this.infoWindow.setContent
+
+        function getDemographics(location) {
+          $.getJSON(
+            "https://www.broadbandmap.gov/broadbandmap/demographic/2014/"+
+            "coordinates?latitude="+location.lat+"&longitude="+
+            location.lng+"&format=json", function(json) {
+            console.log(json.Results);
+            content="<h4>Area Demographics:</h4>"+
+                    "<p>Education: High School Graduate: " +
+                    json.Results.educationHighSchoolGraduate.toFixed(1)+"%</p>"+
+                    "<p>Education: Bachelor's Degree or Greater: " +
+                    json.Results.educationBachelorOrGreater.toFixed(1)+"%</p>"+
+                    "<p>Education: High School Graduate: " +
+                    json.Results.educationHighSchoolGraduate.toFixed(1)+"%</p>"+
+                    "<p>Income Below Poverty: "+
+                    json.Results.incomeBelowPoverty.toFixed(1)+"%</p>"+
+                    "<p>Income less than $25,000: " +
+                    json.Results.incomeLessThan25.toFixed(1)+"%</p>"+
+                    "<p>Income Between $25,000 and $50,000: " +
+                    json.Results.incomeBetween25to50.toFixed(1)+"%</p>"+
+                    "<p>Income Between $50,000 and $100,000: " +
+                    json.Results.incomeBetween50to100.toFixed(1)+"%</p>"+
+                    "<p>Income Between $100,000 and $200,000: " +
+                    json.Results.incomeBetween100to200.toFixed(1)+"%</p>"+
+                    "<p>Income greated than $200,000: " +
+                    json.Results.incomeGreater200.toFixed(1)+"%</p>"+
+                    "<p>Median Income: $" +
+                    json.Results.medianIncome.toFixed(1)+"</p>"
+
+
+
+           }).fail(function(jqXHR, textStatus, errorThrown) { content="<p>Failed to Retrieve Demographics Information</p>"; })
+           .always(function() { location.infoWindow.setContent(content) });;
+        }
+
+        getDemographics(this)
+
+        this.infoWindow.open(map,marker)
+
         }
 
     }))
@@ -102,12 +132,12 @@ function AppViewModel() {
 
 
 function initApp() {
-  var belem={lat: -1.455833, lng: -48.503889}
+  var astoria={lat: 40.7644, lng: -73.9235}
   map = new google.maps.Map(document.getElementById('map'), {
-    center: belem,
+    center: astoria,
     mapTypeId: 'terrain',
     draggable:true,
-    zoom: 12
+    zoom: 14
   });
 ko.applyBindings(new AppViewModel());
 }
